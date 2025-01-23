f56fcde6d32d85e78227c442f9781341f497ca30
Debojyoti-Gho <debojyotighoshmain@gmail.com>
2025-01-23 13:03:34 +0530

    Update attendance_test.py

--- attendance_test.py
+++ attendance_test.py
@@ -639,7 +639,7 @@ device_uuid = get_device_uuid()
 
 # Display the result
 if device_uuid:
-    st.write(f"Your device ID is: {device_uuid}"))
+    st.write(f"Your device ID is: {device_uuid}")
 
 def get_precise_location(api_key=None):
     if api_key:
