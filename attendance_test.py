2d9fb44de997923ea0db9c6d119bdad38dead4c1
Debojyoti-Gho <debojyotighoshmain@gmail.com>
2025-03-05 21:12:16 +0530

    Update attendance_test.py

--- attendance_test.py
+++ attendance_test.py
@@ -1036,7 +1036,6 @@ def is_face_registered(face_blob):
     if not detect_spoof(new_face_path):
         st.error("Possible scam detected! This appears to be a printed photo or screen image.")
         st.info("scroll to the previous form and click the previously clicked button again to take a new picture and procceed further")
-        st.stop()
 
     # Step 2: Face Verification (Check if the face is already registered)
     cursor.execute("SELECT student_face FROM students")
